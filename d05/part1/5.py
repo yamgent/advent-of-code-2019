56fc8d3ed9fef788b882519f0add0a8cad319a7d
Tan Wang Leng <tanwangleng@outlook.com>
2019-12-05 20:16:06 +0800

    Day 5: Simplify part 1

--- d05/part1/5.py
+++ d05/part1/5.py
@@ -1,49 +1,41 @@
+c = []
+
+def get(mode, pos):
+    if mode == 0:
+        return c[c[pos]]
+    return c[pos]
+
+
 def main():
     content = open('5.in').read()
+    global c
     c = [int(x) for x in content.split(',')]
+
     ptr = 0
     while True:
-        opcode = c[ptr]
-        if opcode == 99:
+        val = c[ptr]
+        if val == 99:
             break
-        elif opcode == 3:
+        elif val == 3:
             # input
             c[c[ptr+1]] = int(input())
             ptr += 2
-        elif opcode == 4:
-            # output
-            print(str(c[c[ptr+1]]))
-            ptr += 2
-        elif opcode == 104:
-            print(str(c[ptr+1]))
-            ptr += 2
-        elif opcode == 1:
-            c[c[ptr+3]] = c[c[ptr+1]] + c[c[ptr+2]]
-            ptr += 4
-        elif opcode == 101:
-            c[c[ptr+3]] = c[ptr+1] + c[c[ptr+2]]
-            ptr += 4
-        elif opcode == 1001:
-            c[c[ptr+3]] = c[c[ptr+1]] + c[ptr+2]
-            ptr += 4
-        elif opcode == 1101:
-            c[c[ptr+3]] = c[ptr+1] + c[ptr+2]
-            ptr += 4
-        elif opcode == 2:
-            c[c[ptr+3]] = c[c[ptr+1]] * c[c[ptr+2]]
-            ptr += 4
-        elif opcode == 102:
-            c[c[ptr+3]] = c[ptr+1] * c[c[ptr+2]]
-            ptr += 4
-        elif opcode == 1002:
-            c[c[ptr+3]] = c[c[ptr+1]] * c[ptr+2]
-            ptr += 4
-        elif opcode == 1102:
-            c[c[ptr+3]] = c[ptr+1] * c[ptr+2]
-            ptr += 4
         else:
-            print('Invalid opcode {}'.format(opcode))
-            return
+            op = val % 10
+            para = [int(val/100) % 10, int(val/1000) % 10, int(val/10000) % 10]
+
+            if op == 4:
+                print(str(get(para[0], ptr + 1)))
+                ptr += 2
+            elif op == 1:
+                c[c[ptr + 3]] = get(para[0], ptr + 1) + get(para[1], ptr + 2)
+                ptr += 4
+            elif op == 2:
+                c[c[ptr + 3]] = get(para[0], ptr + 1) * get(para[1], ptr + 2)
+                ptr += 4
+            else:
+                print('Invalid opcode {}'.format(opcode))
+                return
 
 
 if __name__ == '__main__':
