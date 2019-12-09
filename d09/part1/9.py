1191e9592c10fe66ba006fe8c1d0d109be5553cc
Tan Wang Leng <tanwangleng@outlook.com>
2019-12-09 13:22:08 +0800

    Day 9: Add part 1 & 2

---
+++ d09/part1/9.py
@@ -0,0 +1,91 @@
+from itertools import permutations
+
+class Computer:
+    def __init__(self, program):
+        self.program = program.copy()
+        self.program.extend([0] * 1000)
+        self.ptr = 0
+        self.halt = False
+        self.last = 0
+        self.relative = 0
+
+    def get(self, mode, pos):
+        if mode == 2:
+            return self.program[self.relative + self.program[pos]]
+        if mode == 0:
+            return self.program[self.program[pos]]
+        return self.program[pos]
+
+    def set(self, mode, pos, value):
+        if mode == 2:
+            self.program[self.relative + self.program[pos]] = value
+            return
+        # set cannot be in mode 1, so this is mode 0
+        self.program[self.program[pos]] = value
+
+    def run(self, input_values):
+        if self.halt:
+            return True, self.last
+
+        current_input_index = 0
+
+        while True:
+            val = self.program[self.ptr]
+            if val == 99:
+                self.halt = True 
+                return True, self.last
+            else:
+                op = val % 10
+                para = [int(val/100) % 10, int(val/1000) % 10, int(val/10000) % 10]
+
+                if op == 3:
+                    # input
+                    assert current_input_index < len(input_values)
+                    self.set(para[0], self.ptr + 1, input_values[current_input_index])
+                    current_input_index += 1
+                    self.ptr += 2
+                elif op == 4:
+                    self.ptr += 2
+                    self.last = self.get(para[0], self.ptr - 1)
+                    return False, self.last 
+                elif op == 1:
+                    self.set(para[2], self.ptr + 3, self.get(para[0], self.ptr + 1) + self.get(para[1], self.ptr + 2))
+                    self.ptr += 4
+                elif op == 2:
+                    self.set(para[2], self.ptr + 3, self.get(para[0], self.ptr + 1) * self.get(para[1], self.ptr + 2))
+                    self.ptr += 4
+                elif op == 5:
+                    self.ptr = self.get(para[1], self.ptr + 2) if self.get(para[0], self.ptr + 1) != 0 else self.ptr + 3
+                elif op == 6:
+                    self.ptr = self.get(para[1], self.ptr + 2) if self.get(para[0], self.ptr + 1) == 0 else self.ptr + 3
+                elif op == 7:
+                    self.set(para[2], self.ptr + 3, 1 if self.get(para[0], self.ptr + 1) < self.get(para[1], self.ptr + 2) else 0)
+                    self.ptr += 4
+                elif op == 8:
+                    self.set(para[2], self.ptr + 3, 1 if self.get(para[0], self.ptr + 1) == self.get(para[1], self.ptr + 2) else 0)
+                    self.ptr += 4
+                elif op == 9:
+                    self.relative += self.get(para[0], self.ptr + 1)
+                    self.ptr += 2
+                else:
+                    print('Invalid opcode {}'.format(op))
+                    self.halt = True
+                    return True, self.last
+        
+
+def main():
+    # c = [int(x) for x in open('9.in').read().strip().split(',')]
+    c = [int(x) for x in input().split(',')]
+    a = Computer(c)
+
+    while True:
+        halted, result = a.run([1])
+
+        if halted:
+            break
+            
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
